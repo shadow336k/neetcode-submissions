dd20b61f671e4208ea788492577c950eae456356
shadow336k <64402041+shadow336k@users.noreply.github.com>
2026-05-20 13:52:08 +0900

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.py
@@ -0,0 +1,26 @@
+import math
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        if not prices:
+            return None
+        max_profit = 0
+        # find max within range after prices[i]
+        # if max not > 0, ignore this index
+        def findProfit(index, market):
+            market_watch = market[index:]
+            current_price = market_watch[0]
+            max_future_price = max(market_watch)
+            if ((max_future_price - current_price) > 0):
+                return max_future_price - current_price
+            return 0
+        for i in range(len(prices)):
+            current_profit = findProfit(i, prices)
+            if current_profit > max_profit:
+                max_profit = current_profit
+        return max_profit
+# line 10
+# i = 1
+# max = 0
+# input prices = [10,1,5,6,7,1]
+# market_watch = [1,5,6,7,1]
+# output = 
