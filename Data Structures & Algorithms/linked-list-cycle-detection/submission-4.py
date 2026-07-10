4911130272af02c2060dac0f940a886c4241c076
shadow336k <64402041+shadow336k@users.noreply.github.com>
2026-07-10 15:07:08 +0900

    Add: linked-list-cycle-detection - submission-4

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-4.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+from collections import defaultdict
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        if not head:
+            return False
+        visited = defaultdict(int)
+        index = 0
+        while head.next:
+            idt = id(head)
+            print(f"visted node {idt}")
+            print(f"if {visited[idt]} less than {index}...")
+            if visited[idt] and visited[idt] < index:
+                 return True
+            visited[id(head)] = index
+            index = index + 1
+            head = head.next
+            
+        return False
+        
